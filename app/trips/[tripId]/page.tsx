60cf6414a63fb4ffa4532695a3864d57458a2d36
drakkken <lisbj456@gmail.com>
2025-06-13 18:53:48 +0530

    added_real_time_google_map_and_itinerary

--- app/trips/[tripId]/page.tsx
+++ app/trips/[tripId]/page.tsx
@@ -1,24 +1,30 @@
 import { auth } from "@/auth";
 import TripDetailClient from "@/components/TripDetailClient";
 import { prisma } from "@/lib/prisma";
-import React from "react";
 
-const page = async ({ params }: { params: Promise<{ tripId: string }> }) => {
+export default async function TripDetail({
+  params,
+}: {
+  params: Promise<{ tripId: string }>;
+}) {
   const { tripId } = await params;
+
   const session = await auth();
+
   if (!session) {
-    return <div>Please Log in</div>;
+    return <div> Please sign in.</div>;
   }
 
   const trip = await prisma.trip.findFirst({
-    where: { id: tripId },
+    where: { id: tripId, userId: session.user?.id },
+    include: { locations: true },
   });
 
+  console.log(trip);
+
   if (!trip) {
-    return <div>Trip Not Found...</div>;
+    return <div> Trip not found.</div>;
   }
 
   return <TripDetailClient trip={trip} />;
-};
-
-export default page;
+}

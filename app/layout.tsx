32cbf8ae0b080bfaf380d3c69a886d49ce1de37a
drakkken <lisbj456@gmail.com>
2025-07-29 23:01:57 +0530

    added_content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar/Navbar";
+
 import { auth } from "@/auth";
+import Navbar from "@/components/Navbar/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

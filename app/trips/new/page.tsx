5621e64bda54fb00a99e1af22bf9fdfab263cc06
drakkken <lisbj456@gmail.com>
2025-07-30 00:18:42 +0530

    added_uploading

--- app/trips/new/page.tsx
+++ app/trips/new/page.tsx
@@ -1,8 +1,100 @@
 "use client";
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { createTrip } from "@/lib/actions/create-trip";
+import { UploadButton } from "@/lib/upload-thing";
+import { cn } from "@/lib/utils";
+import React, { useState, useTransition } from "react";
 
 const page = () => {
-  return <div></div>;
+  const [isPending, startTransition] = useTransition();
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  return (
+    <div className="max-w-lg mx-auto mt-10">
+      <Card>
+        <CardHeader> New Trip</CardHeader>
+        <CardContent>
+          <form
+            className="space-y-6"
+            action={(formData: FormData) => {
+              //   if (imageUrl) {
+              //     formData.append("imageUrl", imageUrl);
+              //   }
+
+              startTransition(() => {
+                createTrip(formData);
+              });
+            }}
+          >
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                {" "}
+                Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                placeholder="Japan trip..."
+                className={cn(
+                  "w-full border border-gray-300 px-3 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Description
+              </label>
+              <textarea
+                name="description"
+                placeholder="Trip description..."
+                className={cn(
+                  "w-full border border-gray-300 px-3 py-2",
+                  "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                )}
+                required
+              />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  Start Date
+                </label>
+                <input
+                  type="date"
+                  name="startDate"
+                  className={cn(
+                    "w-full border border-gray-300 px-3 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700 mb-1">
+                  {" "}
+                  End Date
+                </label>
+                <input
+                  type="date"
+                  name="endDate"
+                  className={cn(
+                    "w-full border border-gray-300 px-3 py-2",
+                    "rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  )}
+                />
+              </div>
+            </div>
+            <UploadButton endpoint="imageUploader" />
+
+            <Button type="submit" disabled={isPending} className="w-full">
+              {isPending ? "Creating..." : "Create Trip"}
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default page;
